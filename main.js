14ca73c51b686ac9bb956f674a5ce6cbae5410bc
Hassan Achehri <havhassanb@gmail.com>
2025-07-15 21:55:31 +0200

    add

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ let navbar=document.querySelector(".navbar");
 let section=document.querySelectorAll("section");
 let navLinks=document.querySelectorAll("header nav a");
 
+
 window.onscroll=()=>{
      section.forEach(sec=>{
         let top=window.scrollY;
